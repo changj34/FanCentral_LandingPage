18a9e7b11f9965d6e5e9c648177bc50e8dfb14e5
changj34 <changj34@uw.edu>
2021-05-13 15:51:29 -0700

    landing page

--- src/App.js
+++ src/App.js
@@ -1,40 +1,22 @@
 import logo from './images/logo.png';
-import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-} from "react-router-dom";
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Router>
-          <Navbar style={{background: '#3a3b3c', display: 'flex'}} expand="lg" fixed='top'>
-            <Link to="/FanCentral">
-              <img width='60' height='60' alt="idk" src={logo} style={{marginBottom: '0.5rem'}}></img>
-              <Navbar.Brand style={{fontSize: '40px', marginLeft: '20px', marginBottom: '0', fontWeight: 'bold', color: 'white'}}>FanCentral</Navbar.Brand>
-            </Link>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="ml-auto">
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-          
-        </Router>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navbar expand='lg'>
+          <Nav className="mr-auto">
+            <img width='60' height='60' alt="idk" src={logo} style={{marginBottom: '0.5rem'}}></img>
+            <Navbar.Brand style={{fontSize: '40px', marginLeft: '20px', marginBottom: '0', fontWeight: 'bold', color: 'white'}}>FanCentral</Navbar.Brand>
+          </Nav>
+        </Navbar>
+      </header>
+    </div>
+  );
 }
 
 export default App;
