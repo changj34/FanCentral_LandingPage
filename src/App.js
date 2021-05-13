3c26f29daa1b499370a0bfa1c86f70450cbf6f82
changj34 <changj34@uw.edu>
2021-05-13 15:27:34 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import logo from './logo.svg';
+import logo from './images/logo.png';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import {
-  BrowserRouter as Router,
-  Link
-} from "react-router-dom";
 import './App.css';
 
 function App() {
@@ -12,10 +8,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Navbar style={{background: '#3a3b3c', display: 'flex'}} expand="lg" fixed='top'>
-          <Link to="/FanCentral">
-            <img width='60' height='60' src={logo} style={{marginBottom: '0.5rem'}}></img>
-            <Navbar.Brand style={{fontSize: '40px', marginLeft: '20px', marginBottom: '0', fontWeight: 'bold', color: 'white'}}>FanCentral</Navbar.Brand>
-          </Link>
+          <img width='60' height='60' src={logo} style={{marginBottom: '0.5rem'}}></img>
+          <Navbar.Brand style={{fontSize: '40px', marginLeft: '20px', marginBottom: '0', fontWeight: 'bold', color: 'white'}}>FanCentral</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
